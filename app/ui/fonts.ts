8144e0c873e1085daac486175ed0c257a1eee9db
Nolan Bay <nolanbay@Nolans-MacBook-Pro.local>
2024-09-08 20:52:25 -0400

    No fonts

--- app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Inter, Lusitana } from 'next/font/google';
-
-
-export const lusitana = Lusitana ({ subsets: ['latin'], weight: ['400', '700']});
-export const inter = Inter({ subsets: ['latin'] });
